5e9d9ea79cd8628188624160693a44f961f49c6f
haihire <haihire@users.noreply.github.com>
2026-05-11 19:40:51 +0900

    style: @Header 데코레이터 줄바꿈 포매팅

--- server/src/characters/characters.controller.ts
+++ server/src/characters/characters.controller.ts
@@ -6,7 +6,10 @@ export class CharactersController {
   constructor(private readonly charactersService: CharactersService) {}
 
   @Get('stat-builds')
-  @Header('Cache-Control', 'public, s-maxage=86400, stale-while-revalidate=3600')
+  @Header(
+    'Cache-Control',
+    'public, s-maxage=86400, stale-while-revalidate=3600',
+  )
   findStatBuilds() {
     return this.charactersService.findStatBuilds();
   }
--- server/src/sites/sites.controller.ts
+++ server/src/sites/sites.controller.ts
@@ -6,7 +6,10 @@ export class SitesController {
   constructor(private readonly sitesService: SitesService) {}
 
   @Get()
-  @Header('Cache-Control', 'public, s-maxage=86400, stale-while-revalidate=3600')
+  @Header(
+    'Cache-Control',
+    'public, s-maxage=86400, stale-while-revalidate=3600',
+  )
   findAll() {
     return this.sitesService.findAll();
   }
